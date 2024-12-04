5aef97013db444ea1af52acf08eea559761e0665
VictorTubei <vktubei@gmail.com>
2024-12-04 18:47:39 +0300

    completed control flow

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if(distance <=400 ){
+    return 'This one is on me!'
+  } else if(distance >=400 && distance <=2000){
+    return 'That will be twenty bucks.'
+  } else if(distance > 2000 && distance <2500){
+    return 'I will gladly take your thirty bucks.'
+  } else if(distance > 2500){
+    return 'No can do.';
+  }
+    
+}
+function ternaryCheckCity(iscity){
+  // Write your code here!
+  const city= 'NYC'
+  return (iscity === city ? 'Ok, sounds good.' : 'No go.')
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  let generousTip, notgenerousTip;
+  switch (tip){
+    case 'generous':{
+      return 'Thank you so much.'
+      break;
+    }
+    case 'not as generous': {
+      return 'Thank you.'
+    }
+    default:{
+      return 'Bye.'
+    }
+  }
 }
